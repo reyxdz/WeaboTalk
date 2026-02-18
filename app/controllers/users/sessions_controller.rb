07e7ad18df2f787953908dba313244d072c1c98f
reyxdz <reyfdenzo@outlook.com>
2026-02-18 14:17:11 +0800

    Improve UX for unconfirmed email sign-in attempts
    - Update SessionsController to check if user is unconfirmed before sign-in
    - Redirect unconfirmed users to resend confirmation email page with helpful message
    - Add alert message display in confirmation page when redirected from sign-in
    - Add info banner on sign-in page explaining email confirmation requirement
    - Better user guidance when trying to sign in with unconfirmed email

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,5 +3,26 @@
 module Users
   class SessionsController < Devise::SessionsController
     layout "devise"
+
+    # POST /resource/sign_in
+    def create
+      # Check if user exists but is unconfirmed
+      user = User.find_by(email: user_params[:email])
+      
+      if user && !user.confirmed?
+        # User exists but hasn't confirmed email yet
+        redirect_to new_user_confirmation_path(email: user.email), 
+                    alert: "Please confirm your email first. We've sent you a confirmation link."
+        return
+      end
+
+      super
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :remember_me)
+    end
   end
 end
