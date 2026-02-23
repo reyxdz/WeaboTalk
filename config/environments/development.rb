81527f277185baf9e75241d87c9399df6488362b
reyxdz <reyfdenzo@outlook.com>
2026-02-23 19:35:23 +0800

    Fix: Add protocol scheme to email URL generation for proper confirmation links

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000, protocol: "http" }
 
   # Gmail SMTP configuration for sending real emails
   config.action_mailer.delivery_method = :smtp
